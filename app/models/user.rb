d1bcd5eb99435e32abe795dd64d5682e9f1bd42a
Mihndim <jelsieprisca2016@gmail.com>
2021-04-06 15:15:24 -0400

    Add user to the db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates: username, presence: true, uniqueness: true, length: {in: 4..16}
-  validates: password, presence: true, length: {in: 6..16} 
-  validates: email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: {in: 6..24}
+  validates :password, presence: true, length: {in: 8..24} 
+  validates :email, presence: true, uniqueness: true
 end
